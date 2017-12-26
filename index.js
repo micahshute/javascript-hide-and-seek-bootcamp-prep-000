e1bb5e083ef22d07e76829553e410074f2bd3fc6
Micah Shute <micah.shute@gmail.com>
2017-12-26 19:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,71 +14,13 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var allNodes = deepestOfBranch(document.querySelectorAll('div#grand-node'));
-  var deepestCounter = 0;
-  var returnVal;
-  for(let i = 0; i < allNodes.length; i++){
-    var keys = Object.keys(allNodes[i]);
-    for(let j = 0; j < keys.length; j++){
-      if(keys[j] > deepestCounter){
-        deepestCounter = keys[j];
-        returnVal = allNodes[i][keys[j]];
-      }else{
-        returnVal = globalTest
-      }
+  var patriarch = document.querySelector('div#grand-node')
+  var nextLevel = patriarch.querySelector('*')
+  while(nextLevel){
+    if(nextLevel.querySelector('*')){
+      nextLevel = nextLevel.querySelector('*')
+    }else{
+      return nextLevel
     }
   }
-  return returnVal;
 }
-
-var globalTest;
-
-function deepestOfBranch(branch, startingLvl){
-  var deepestOfNodes = [{}];
-  var counter = startingLvl;
-  for(let i = 0; i <branch.length; i++){
-    var current = branch[i].querySelectorAll(' >*');
-    counter++;
-    var placeholder = 'NONE';
-    while(current && current.length > 0){
-      if(current && current.length > 1){
-        for(var obj in deepestOfBranch(current, counter)){
-          deepestOfNodes.push(obj);
-        }
-        break;
-      }else if (current.length === 1){
-        placeholder = current[0]
-        current = current[0].querySelectorAll('*>*');
-        counter++;
-      }
-    }
-    deepestOfNodes[0][counter] = placeholder;
-  }
-  return deepestOfNodes;
-}
-
-// function deepestChild(){
-//   var allNodes = deepestOfBranch(document.querySelectorAll('div#grand-node'))
-//   var deepestCounter = 0;
-//   var returnVal;
-//   var keys = Object.keys(allNodes)
-//     for(let j = 0; j < keys.length; j++){
-//       if(keys[j] > deepestCounter){
-//         deepestCounter = keys[j]
-//         returnVal = allNodes[keys[j]]
-//         console.log(returnVal)
-//       }
-//     }
-//   return returnVal
-// }
-
-
-// function deepestOfBranch(branch){
-//   var deepestOfNodes = {};
-//   for(let i = 0; i <branch.length; i++){
-//     var current = branch[i].querySelectorAll(':last-child')
-//     deepestOfNodes = current
-//   }
-//   return deepestOfNodes
-// }
-
